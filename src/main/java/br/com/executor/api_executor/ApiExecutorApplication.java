0c23a69fec1958a51d496a53ec80a4631afef409
Danilo Silva <danilors.work@gmail.com>
2025-05-24 13:13:22 -0300

    add java project files

---
+++ src/main/java/br/com/executor/api_executor/ApiExecutorApplication.java
@@ -0,0 +1,13 @@
+package br.com.executor.api_executor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiExecutorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiExecutorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/executor/api_executor/ApiExecutorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.executor.api_executor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiExecutorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
